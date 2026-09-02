2cab04f3d6499b3a8e767dee62a75ca211171f8e
catboylei <243333633+catboylei@users.noreply.github.com>
2026-09-02 19:09:30 +0200

    fix previous commit because it didnt work

--- meowvim/lua/lei/leiInit.lua
+++ meowvim/lua/lei/leiInit.lua
@@ -1,22 +1,21 @@
--- init persistent global var
 vim.opt.shada:append("!")
-vim.g.ENABLE_LEI = vim.g.ENABLE_LEI
-if vim.g.ENABLE_LEI == nil then
-    vim.g.ENABLE_LEI = false
-end
 
 vim.api.nvim_create_user_command("ToggleLei", function()
     vim.g.ENABLE_LEI = not vim.g.ENABLE_LEI
     print("ENABLE_LEI = " .. tostring(vim.g.ENABLE_LEI))
 end, {})
 
-local enable = vim.g.ENABLE_LEI
+vim.schedule(function()
+    if vim.g.ENABLE_LEI == nil then
+        vim.g.ENABLE_LEI = false
+    end
 
--- my stuff
-if (enable) then
-    -- below is an attempt at reproducing kakoune/helix motions as they are superior
-    -- obviously wont be as good because of the visual mode shenanigans and everything
-    require("helixRemap")
-
-
-end
+    -- my stuff goes down here
+    if vim.g.ENABLE_LEI then
+        -- below is an attempt at reproducing kakoune/helix motions as they are superior
+        -- obviously wont be as good because of the visual mode shenanigans and everything
+        require("lei.helixRemap")
+        -- other personal changes like keybinds or stuff
+        require("lei.evilLeiTweaks")
+    end
+end)
