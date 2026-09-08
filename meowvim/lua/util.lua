a027553847f0caa1fd2700f6f2ecde43543cd01f
itsyunaya <40719746+itsyunaya@users.noreply.github.com>
2026-09-08 16:42:16 +0200

    refactor: make unbundled ls checker utility function

---
+++ meowvim/lua/util.lua
@@ -0,0 +1,17 @@
+--- Utility functions
+U = {}
+
+-- since i don't want to bundle every language server i use, we simply
+-- expect them to be in a devshell. if they're not, we send a notification
+-- to inform the user
+--- @param ls string
+function U.unbundled_ls_check(ls)
+	if vim.fn.executable(ls) == 0 then
+		vim.notify("Language server " .. ls .. " is unavailable")
+	else
+		local ls_name = ls:gsub("-", "_")
+		vim.lsp.enable(ls_name)
+	end
+end
+
+return U
