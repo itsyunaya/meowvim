992632988c018e7c0eb1f336069d5bcf0f63ad14
itsyunaya <40719746+itsyunaya@users.noreply.github.com>
2026-09-17 19:40:42 +0200

    fix: conditionally disable session-manager

---
+++ meowvim/plugin/session-manager.lua
@@ -0,0 +1,8 @@
+-- lei plugin which i don't like using
+if not vim.g.ENABLE_LEI then
+	local config = require('session_manager.config')
+	require("session_manager").setup({
+		autoload_mode = config.AutoloadMode.Disabled,
+		autosave_last_session = false,
+	})
+end
